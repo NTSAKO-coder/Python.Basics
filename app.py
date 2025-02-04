208f3876344783235690ab06d34b63fe943e7a27
Ntsako-Coder <janelntsako@gmail.com>
2025-02-04 12:01:52 +0200

    print added

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 print('Hello Ntsako') # first line code
 
-name = input("What's on your mind?")
+name = input("What's your name?")
 age = input('How old are you?')
+
+print("Hey Ntsako" + name)
+print("You are 30" + age + "?")
 
