f26ecc8ae3903088cb909080b565952d19234e4b
Ntsako-Coder <janelntsako@gmail.com>
2025-02-07 11:47:27 +0200

    conditional statements

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-print('Hello Ntsako') # first line code
+#print('Hello Ntsako') # first line code
 
-name = input("What's your name?")
-age = input('How old are you?')
+#name = input("What's your name?")
+#age = input('How old are you?')
 
-print("Hey Ntsako" + name)
-print("You are 30" + age + "?")
+#print("Hey Ntsako" + name)
+#print("You are 30" + age + "?")
+
+#conditional statement#
+
+X = 2
+
+if X > 2:
+    print("you've gotten only 23")
+elif X ==5:
+    print("gotten just 2")
+    
+else:X
+print("such a low score")
 
