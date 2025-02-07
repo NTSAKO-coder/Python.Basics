e89ed3860464afd042b0ccff9cb37ec1d40c33f5
Ntsako-Coder <janelntsako@gmail.com>
2025-02-07 13:04:30 +0200

    loops added

--- app.py
+++ app.py
@@ -27,18 +27,21 @@
 #else:
     #print("Access denied. You must be at least 18 years old.")
     
-age = int(input("Enter your age: "))
+#age = int(input("Enter your age: "))
 
-if age >= 18:
-    has_id = input("Do you have an ID? (yes/no): ")
+#if age >= 18:
+    #has_id = input("Do you have an ID? (yes/no): ")
     
-    if has_id == "yes":
-        print("Access granted.")
-    else:
-        print("Access denied. You need an ID.")
-else:
-    print("Access denied. You must be at least 18 years old.")
+    #if has_id == "yes":
+        #print("Access granted.")
+    #else:
+        #print("Access denied. You need an ID.")
+#else:
+    #print("Access denied. You must be at least 18 years old.")
     
 
+fruits = ['banana', 'apple', 'peaches','Mango','Grapes']
+for fruit in fruits:
+            print(fruits)
     
 
